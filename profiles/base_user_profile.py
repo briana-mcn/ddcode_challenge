107bcfc796c2e107715c16b9a26b1dc2455662a2
briana-mcn <briana.mcn@gmail.com>
2018-07-19 14:30:38 -0500

    master: Move UserProfile dictionary to class-level
    This implementation allows for sub-classes to append to the dictionary

--- profiles/base_user_profile.py
+++ profiles/base_user_profile.py
@@ -3,39 +3,41 @@ class UserProfile:
     This class retrieves the required data pertinent for the code challenge and builds
     an appropriate response dictionary with the required
     """
-    def __init__(self):
-        self.retrieved_data = {
-            "repos": {
-                "original": {
-                    "names": [],
-                    "count": 0,
-                    "repo_watchers": 0,
-                    "commit_count": 0
-                },
-                "forked": {
-                    "names": [],
-                    "count": 0
-                }
-            },
-            "open_issues_count": 0,
-            "account_size": 0,
-            "languages": {
+    # this is not the best implementation as it's not clearly indicative of the state of the program.
+    # a subclass could inherit this class, expecting the 'retrieved_data' dictionary to be identical,
+    # but it could be out of sync if somewhere else in the program child classes were manipulating this dict
+    retrieved_data = {
+        "repos": {
+            "original": {
                 "names": [],
-                "count": 0
+                "count": 0,
+                "repo_watchers": 0,
+                "commit_count": 0
             },
-            "reputation": {
-                "followers": 0,
-                "following": 0
-            },
-            "stars": {
-                "given": 0,
-                "received": 0
-            },
-            "repo_topics": {
+            "forked": {
                 "names": [],
                 "count": 0
             }
+        },
+        "open_issues_count": 0,
+        "account_size": 0,
+        "languages": {
+            "names": [],
+            "count": 0
+        },
+        "reputation": {
+            "followers": 0,
+            "following": 0
+        },
+        "stars": {
+            "given": 0,
+            "received": 0
+        },
+        "repo_topics": {
+            "names": [],
+            "count": 0
         }
+    }
 
     def calculate_total_profile_data(self):
         self.calculate_languages()
